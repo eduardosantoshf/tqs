695d1b589b463eac302306106df15eaa0442f199
Eduardo Santos <eduardosantoshf@gmail.com>
2021-03-17 00:03:19 +0000

    Half of the tests from 1 made

--- lab01/junit5-jupiter-starter-maven/src/main/java/com/example/project/TQSSimpleStack.java
+++ lab01/junit5-jupiter-starter-maven/src/main/java/com/example/project/TQSSimpleStack.java
@@ -1,19 +1,60 @@
 package com.example.project;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class TQSSimpleStack<T> {
     private ArrayList<T> stack;
+    private int maxStackSize;
 
-    public TQSSimpleStack(){ this.stack = new ArrayList<>(); }
+    public TQSSimpleStack(){
+        this.stack = new ArrayList<T>();
+        this.maxStackSize = -1;
+    }
 
-    public void push(T n){ this.stack.add(n); }
+    public TQSSimpleStack(int maxSize){
+        if (maxSize < 1) {
+            throw new IllegalArgumentException("ERROR! The stack maximum size must be > 0!");
+        }
+        this.stack = new ArrayList<T>();
+        this.maxStackSize = maxSize;
+    }
 
-    public T pop(){ return this.stack.remove(this.stack.size() - 1); }
+    public void push(T n){
+        if (this.maxStackSize > 0) {
+            if (this.stack.size() < maxStackSize) {
+                this.stack.add(n);
+            } else {
+                throw new IllegalStateException("ERROR!");
+            }
+        } else {
+            this.stack.add(n);
+        }
+        
+        this.stack.add(n);
+    }
 
-    public T peek(){ return this.stack.get(this.stack.size() - 1); }
+    public T pop(){
+        if (this.stack.size() == 0) {
+            throw new NoSuchElementException("ERROR! There is nothing to remove!");
+        } else {
+            return this.stack.remove(this.stack.size() - 1);
+        }
+    }
 
-    public int size(){ return this.stack.size(); }
+    public T peek(){
+        if (this.stack.size() == 0) {
+            throw new NoSuchElementException("ERROR! There are no elements in this stack!");
+        } else {
+            return this.stack.get(this.stack.size() - 1);
+        }
+    }
 
-    public boolean isEmpty(){ return this.stack.size() == 0; }
+    public int size(){
+        return this.stack.size();
+    }
+
+    public boolean isEmpty(){
+        return this.stack.size() == 0;
+    }
 }
--- lab01/junit5-jupiter-starter-maven/src/test/java/com/example/project/TQSSimpleStackTest.java
+++ lab01/junit5-jupiter-starter-maven/src/test/java/com/example/project/TQSSimpleStackTest.java
@@ -3,6 +3,8 @@ package com.example.project;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TQSSimpleStackTest {
@@ -19,6 +21,7 @@ public class TQSSimpleStackTest {
         testStack.push("Coimbra");
     }
 
+    /*
     @Test
     void isEmpty(){ assertTrue(emptyStack.isEmpty(), "Empty stack should report empty!"); }
 
@@ -43,5 +46,46 @@ public class TQSSimpleStackTest {
         testStack.push("Porto");
         assertEquals("Porto", testStack.peek());
         assertEquals(4, testStack.size());
+    }
+    */
+
+    @Test
+    @DisplayName("Stack is empty on construction")
+    public void emptyOnConstruction() {
+        assertTrue(emptyStack.isEmpty(), "Stack must be empty on construction!");
+    }
+
+    @Test
+    @DisplayName("Stack has size 0 on construction")
+    public void SizeZeroOnConstruction() {
+        assertTrue(emptyStack.size() == 0, "Stack must have size 0 on construction!");
+    }
+
+    @Test
+    @DisplayName("After n pushes to an empty stack, n > 0, the stack is not empty and its size is n")
+    public void sizeAfterPushes() {
+        assertEquals(3, testStack.size(), "The sixe is not the expected one!");
+    }
+
+    @Test
+    @DisplayName("If one pushes x then pops, the value popped is x")
+    public void pushThenPop() {
+        testStack.push("Porto");
+        assertEquals("Porto", testStack.pop(), "The popped value is incorrect!");
+    }
+
+    @Test
+    @DisplayName("If one pushes x then peeks, the value returned is x, but the size stays the same")
+    public void pushThenPeek() {
+        int stackSize = testStack.size();
+        testStack.push("Faro");
+        assertEquals("Faro", testStack.peek(), "The peeked value is incorrect!");
+        assertEquals(stackSize, testStack.size(), "The size is not the same after peeking!");
+    }
+
+    @Test
+    @DisplayName("If the size is n, then after n pops, the stack is empty and has a size 0")
+    public void emptyAfterPops() {
+        
     }
 }
