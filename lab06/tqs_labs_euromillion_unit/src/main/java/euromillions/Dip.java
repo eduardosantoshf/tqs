e8d2b09863db764c0125b8d1b92e1069fd238dc5
Eduardo Santos <eduardosantoshf@gmail.com>
2021-05-01 21:43:03 +0100

    Lab6.2_b finished

---
+++ lab06/tqs_labs_euromillion_unit/src/main/java/euromillions/Dip.java
@@ -0,0 +1,115 @@
+package euromillions;
+
+import java.util.Objects;
+
+import sets.SetOfNaturals;
+
+import java.util.Random;
+
+/**
+ * A set of 5 numbers and 2 starts according to the Euromillions ranges.
+ *
+ * @author ico0
+ */
+public class Dip {
+
+    final static int MAX_NUMBER_OF_NUMBERS = 5;
+    final static int MAX_NUMBER_OF_STARS = 2;
+
+    final static int NUMBER_LIMIT = 49;
+    final static int STAR_LIMIT = 9;
+
+    private SetOfNaturals numbers;
+    private SetOfNaturals starts;
+
+    public Dip() {
+        numbers = new SetOfNaturals();
+        starts = new SetOfNaturals();
+    }
+
+    public Dip(int[] arrayOfNumbers, int[] arrayOfStarts) {
+        this();
+
+        if (arrayOfNumbers.length == MAX_NUMBER_OF_NUMBERS && arrayOfStarts.length == MAX_NUMBER_OF_STARS) {
+            numbers.add(arrayOfNumbers);
+            starts.add(arrayOfStarts);
+        } else {
+            throw new IllegalArgumentException("wrong number of elements in numbers/stars");
+        }
+
+    }
+
+    public SetOfNaturals getNumbersColl() {
+        return numbers;
+    }
+
+    public SetOfNaturals getStarsColl() {
+        return starts;
+    }
+
+    public static Dip generateRandomDip() {
+        Random generator = new Random();
+
+        Dip randomDip = new Dip();
+        for (int i = 0; i < 5; i++) {
+            int candidate = generator.nextInt(NUMBER_LIMIT) + 1;
+            if (!randomDip.getNumbersColl().contains(candidate)) {
+                randomDip.getNumbersColl().add(candidate);
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            int candidate = generator.nextInt(STAR_LIMIT) + 1;
+            if (!randomDip.getStarsColl().contains(candidate)) {
+                randomDip.getStarsColl().add(candidate);
+            }
+        }
+        return randomDip;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.numbers);
+        hash = 29 * hash + Objects.hashCode(this.starts);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Dip other = (Dip) obj;
+        if (!Objects.equals(this.numbers, other.numbers)) {
+            return false;
+        }
+        return Objects.equals(this.starts, other.starts);
+    }
+
+
+    /**
+     * prepares a string representation of the data structure, formated for
+     * printing
+     *
+     * @return formatted string with data
+     */
+    public String format() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("N[");
+        for (int number : getNumbersColl()) {
+            sb.append(String.format("%3d", number));
+        }
+        sb.append("] S[");
+        for (int star : getStarsColl()) {
+            sb.append(String.format("%3d", star));
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
